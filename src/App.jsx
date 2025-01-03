e23bdb6892aeb0d7487e45803d755144f920e32a
firat <firat.duzgun@hotmail.com>
2025-01-03 18:32:45 +0300

    local project update

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Header from './components/Header.jsx';
+import Results from './components/Results.jsx';
+import UserInput from './components/UserInput.jsx';
+
+function App() {
+  const [userInput, setuserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  const inputIsValid =
+    userInput.duration > 0 &&
+    userInput.expectedReturn > 0 &&
+    userInput.annualInvestment > 0 &&
+    userInput.initialInvestment > 0;
+
+  function handleChange(inputIdentifier, newValue) {
+    setuserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter positive values</p>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img src={logo} alt="a moneybag filled with gold" />
+      <h1>ZAMMIR HESSAPLAYICI</h1>
+    </header>
+  );
+};
+
+export default Header;
